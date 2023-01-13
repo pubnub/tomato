e1b0828a5262bb72b0cdddff50dfd8ea79fbdcb7
Are <me@iama.re>
2023-01-13 10:56:10 +0100

    feat: adds asRecord to StringMatcher (#9)

--- src/components/assertions/string-matcher.ts
+++ src/components/assertions/string-matcher.ts
@@ -1,6 +1,7 @@
 import { Matcher } from './matcher.js'
 import { ArrayMatcher } from './array-matcher.js'
 import { NumberMatcher } from './number-matcher.js'
+import { RecordMatcher } from './record-matcher.js'
 
 export class StringMatcher<O> extends Matcher<O, string> {
   startsWith = this.makeAssertion(
@@ -52,6 +53,14 @@ export class StringMatcher<O> extends Matcher<O, string> {
     return new NumberMatcher(
       this.description,
       this.zoom((value) => parseFloat(value)),
+      this.reverseLens
+    )
+  }
+
+  get asRecord() {
+    return new RecordMatcher(
+      this.description,
+      this.zoom((value) => JSON.parse(value)),
       this.reverseLens
     )
   }
