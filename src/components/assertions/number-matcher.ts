688a2166dfff4dc473fea6a41b25cda7f4b4d3c6
Artur Wojciechowski <me@iama.re>
2023-01-24 10:43:13 +0100

    feat: require value to not be null or undefined in some assertions

--- src/components/assertions/number-matcher.ts
+++ src/components/assertions/number-matcher.ts
@@ -3,30 +3,37 @@ import { Matcher } from './matcher.js'
 export class NumberMatcher<O> extends Matcher<O, number> {
   between = this.makeAssertion(
     (actual, min: number, max: number) => actual >= min && actual <= max,
-    (min, max) => `be between ${min} and ${max}`
+    (min, max) => `be between ${min} and ${max}`,
+    true
   )
   gt = this.makeAssertion(
     (actual, value: number) => actual > value,
-    (value) => `be greater than ${value}`
+    (value) => `be greater than ${value}`,
+    true
   )
   gte = this.makeAssertion(
     (actual, value: number) => actual >= value,
-    (value) => `be greater than or equal to ${value}`
+    (value) => `be greater than or equal to ${value}`,
+    true
   )
   lt = this.makeAssertion(
     (actual, value: number) => actual < value,
-    (value) => `be less than ${value}`
+    (value) => `be less than ${value}`,
+    true
   )
   lte = this.makeAssertion(
     (actual, value: number) => actual <= value,
-    (value) => `be less than or equal to ${value}`
+    (value) => `be less than or equal to ${value}`,
+    true
   )
   equals = this.makeAssertion(
     (actual, value: number) => actual === value,
-    (value) => `be equal to ${value}`
+    (value) => `be equal to ${value}`,
+    true
   )
   around = this.makeAssertion(
     (actual, value: number, epsilon: number = 0.01) => Math.abs(actual - value) < epsilon,
-    (value) => `be approximately equal to ${value}`
+    (value) => `be approximately equal to ${value}`,
+    true
   )
 }
